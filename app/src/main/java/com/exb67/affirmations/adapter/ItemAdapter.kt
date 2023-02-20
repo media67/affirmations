9c567caa4c945809cfcf68d98c62c6016ea3c7f6
Samuel Bourassa <district67@gmail.com>
2023-02-20 16:24:57 -0500

    Add ImageView to display an image along with each affirmation.

--- app/src/main/java/com/exb67/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/exb67/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.exb67.affirmations.R
@@ -21,9 +22,11 @@ class ItemAdapter(
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
+
 
     /**
      * Create new views (invoked by the layout manager)
@@ -42,7 +45,9 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
+
 
     /**
      * Return the size of your dataset (invoked by the layout manager)
